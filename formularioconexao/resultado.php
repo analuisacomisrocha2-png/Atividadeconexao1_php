90556e420b23a482ca7f1487764cec4d43114416
Ana Luisa <analuisacomisrocha2@gmail.com>
2026-07-02 16:08:34 -0300

    soma

---
+++ formularioconexao/resultado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+</head>
+
+<body>
+   
+    <h1>Resultado da Soma</h1>
+    <?php
+
+   
+    $num1 = $_POST['numero1'];
+    $num2 = $_POST['numero2'];
+    $soma = $num1 + $num2;
+
+$sql ="INSERT INTO soma (numero1, numero2, resultado)VALUES ($num1, $num2, $soma)";
+
+$conexao->query($sql);
+
+echo "O resultado é $soma";
+
+    ?>
+</body>
+
+</html>
