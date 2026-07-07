c3a5ce4f0ed960000beeacbb0dee02512dfc0fe7
Ana Luisa <analuisacomisrocha2@gmail.com>
2026-07-07 15:19:29 -0300

    f

--- conexao/atividade22/resultado.php
+++ conexao/atividade22/resultado.php
@@ -18,10 +18,12 @@
    
     $numero1 = $_POST['numero1'];
     $numero2 = $_POST['numero2'];
-    $subtracao = $numero1 - $numero2 ;
+    $numero1 = $_POST['numero3'];
+    $numero2 = $_POST['numero4'];
+    $subtracao = $numero1 - $numero2 - $numero3 - $numero4 ;
 
     //sql 
-    $sql = "INSERT INTO subtracao (num1, num2, resultado) VALUES ($numero1, $numero2,  $subtracao)";
+    $sql = "INSERT INTO subtracao (num1, num2,, num3, num4, resultado) VALUES ($numero1, $numero2, $numero3, $numero4  $subtracao)";
 
     $conexao->query($sql);
 
