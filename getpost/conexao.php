42e6abcc74dac74fab017b205bc22826a24abcd7
Ana Luisa <analuisacomisrocha2@gmail.com>
2026-07-21 14:57:42 -0300

    h

---
+++ getpost/conexao.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$senha = "senac";
+$banco = "conexaomultphp";
+$porta = "3307";
+
+$conexao = new mysqli(
+$host, 
+$usuario, 
+$senha, 
+$banco, 
+$porta
+);
+
+?> 
