e629a7f28f9a3582339b3fcc761a181196ec3db7
Ana Luisa <analuisacomisrocha2@gmail.com>
2026-07-21 15:25:00 -0300

    a

--- atividade23/atualizar_cliente.php
+++ atividade23/atualizar_cliente.php
@@ -1,4 +1,30 @@
 <?php 
+include "banco.php";
+
+$nome=$_POST['nome'];
+$idade=$_POST['idade'];
+$email=$_POST['email'];
+$telefone=$_POST['telefone'];
+$cidade=$_POST['cidade'];
+
+
+$sql= "UPDATE sistema
+SET 
+nome:'$nome';
+idade:'$idade';
+email:'$email';
+telefone:'$telefone';
+cidade:'$cidade';
+WHERE id = $id";
+
+if ($conexao-> real_query($sql)) {
+    header("location : listar_cliente.php");
+
+      exit();
+
+}else {
+    echo "Erro ao atualizar o cliente..." . $conexao->error;
+}
 
 
 
--- atividade23/listar_cliente.php
+++ atividade23/listar_cliente.php
@@ -16,6 +16,13 @@ while ($cliente = $resultado->fetch_assoc()){
 echo "Nome:".$cliente['nome']."<br>";
 echo "Idade:".$cliente['idade']."<br>";
 echo "Email:".$cliente['email']."<br>";
+echo "Telefone".$cliente['telefone']."<br>";
+echo "Cidade".$cliente['cidade']."<br>";
+
+echo "<a href='editar_cliente.php?id= ". $cliente['id'] . "'> <button> Editar </button> <a/>";
+echo "<a href='excluir_cliente.php?id= ". $cliente['id'] . "'> <button> Excluir </button> <a/>";
+
+echo "<hr>";
 
 }
 
