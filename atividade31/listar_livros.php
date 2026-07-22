e4ac9808c11d795602318e65897bd89cc4fe2ea5
Ana Luisa <analuisacomisrocha2@gmail.com>
2026-07-22 16:23:03 -0300

    d

---
+++ atividade31/listar_livros.php
@@ -0,0 +1,28 @@
+<?php 
+//incluindo a conexao
+include "banco.php";
+
+//sql para selecionar todos os registros
+$sql="SELECT * FROM form";
+
+//criamos a variavel $resultado para guardar a consulta
+$resultado = $conexao->query($sql);
+
+//laço de repetição while(enquanto)
+//fetch_assoc busca o registro no banco
+while ($produto = $resultado->fetch_assoc()){
+
+//mostro o campo nome da tabela form
+echo "Titulo:".$cliente['titulo']."<br>";
+echo "Autor:".$cliente['autor']."<br>";
+echo "Editora:".$cliente['departamento']."<br>";
+echo "Ano da publicacao:".$cliente['salario']."<br>";
+echo "Entrega:".$cliente['entrega']."<br>";
+
+echo "<a href='funcionario.php?id= ". $cliente['id'] . "'> <button> Enviar </button> <a/>";
+
+echo "<hr>";
+
+}
+
+?>
